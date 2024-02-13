04a7c99e74b3bf12e380888b437c8709ae021227
David Franco <115614773+dfranco100@users.noreply.github.com>
2024-02-13 00:04:31 -0600

    Add files via upload

---
+++ ViewModel/MailSettings.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FitnessPro.ViewModel
+{
+    public class MailSettings
+    {
+        //configure and use smtp server from google 
+
+        public string? Email { get; set; }
+        public string? DisplayName { get; set; }
+        public string? Password { get; set; }
+        public string? Host {get;set;}
+        public int Port { get; set; }
+    }
+}
